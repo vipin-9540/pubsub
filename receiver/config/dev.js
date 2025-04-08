d00d0376b9ec4e99f949ba9a2521c46f9dcb1384
vipin singh <vipinsingh@vipins-MacBook-Air.local>
2025-04-08 15:39:45 +0530

    add missing dependency

---
+++ receiver/config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "db": {
+      "host": "localhost",
+      "port": 27017,
+      "name": "pubsub",
+      "client": "mongodb://mongo:27017/pubsub",
+    },
+    "redis": {
+      "url": "redis://redis:6379"
+    },
+    "port": 3000
+  };
